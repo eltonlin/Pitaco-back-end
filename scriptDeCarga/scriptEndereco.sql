c942188e65716ff5c415c7f19cf2795e759f7725
FernandoBCAraujo <48303044+FernandoBCAraujo@users.noreply.github.com>
2019-03-12 22:33:36 -0300

    Update and rename scriptEndereco to scriptEndereco.sql

---
+++ scriptDeCarga/scriptEndereco.sql
@@ -0,0 +1,8 @@
+INSERT INTO endereco (usuario_final, rua, cep, bairro, complemento)
+				             VALUES ("fernando", "Rua do Hospicio 111", "50060080", "Boa Vista", "Apt 605"),
+                            ("renato", "Rua do Beira Rio 777", "50070080", "Beira Rio", "Apt 900"),
+                            ("rafael", "Rua da Torre 555", "55060080", "Torre", "Apt 505"),
+                            ("juliane", "Rua da Boa Viagem 131", "30060080", "Boa Viagem", "Apt 305"),
+                            ("jedesson", "Rua do Camaragibe 411", "50060089", "Camaragibe", "Apt 105"),
+                            ("adriano", "Rua do Cabo 333", "57060080", "Cabo", "Apt 205"),
+                            ("elton", "Rua do Jordao 110", "51160080", "Jordao, "Apt 600");
