b849bbaecd0b265b603ab8e63734f2aa42f0dd1e
FernandoBCAraujo <48303044+FernandoBCAraujo@users.noreply.github.com>
2019-03-12 22:41:12 -0300

    Update scriptEndereco.sql

--- scriptDeCarga/scriptEndereco.sql
+++ scriptDeCarga/scriptEndereco.sql
@@ -4,5 +4,5 @@ INSERT INTO endereco (usuario_final, rua, cep, bairro, complemento)
                             ("rafael", "Rua da Torre 555", "55060080", "Torre", "Apt 505"),
                             ("juliane", "Rua da Boa Viagem 131", "30060080", "Boa Viagem", "Apt 305"),
                             ("jedesson", "Rua do Camaragibe 411", "50060089", "Camaragibe", "Apt 105"),
-                            ("adriano", "Rua do Cabo 333", "57060080", "Cabo", "Apt 205"),
+                            ("adriano", "Rua do Cabo Duro 333", "57060080", "Cohab", "Apt 205"),
                             ("elton", "Rua do Jordao 110", "51160080", "Jordao, "Apt 600");
