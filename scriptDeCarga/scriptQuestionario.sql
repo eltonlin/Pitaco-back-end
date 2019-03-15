0de616370f6558a294c9179ffb46c6308af52eb0
FernandoBCAraujo <48303044+FernandoBCAraujo@users.noreply.github.com>
2019-03-15 01:24:10 -0300

    Delete scriptQuestionario.sql

--- scriptDeCarga/scriptQuestionario.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO questionario (descricao_questionario, pontuacao_questionario, usuario_master, id_empresa, id_interesse)
-                  VALUES ("Sobre bebidas", 1000, "Fernando Master", 1, 6),
-                         ("Sobre comida", 700, "Renato Master", 2, 5),
-                         ("Sobre música", 900, "Rafael Master", 4, 1),
-                         ("Sobre esportes", 500, "Juliane Master", 5, 2),
-                         ("Sobre carros", 300, "Jedesson Master", 7, 3);
