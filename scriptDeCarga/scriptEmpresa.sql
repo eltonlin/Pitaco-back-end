dc35d80701045ac0ca68a4f96d51fb83e889a224
FernandoBCAraujo <48303044+FernandoBCAraujo@users.noreply.github.com>
2019-03-15 01:25:38 -0300

    Delete scriptEmpresa.sql

--- scriptDeCarga/scriptEmpresa.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO empresa (nome_empresa, cnpj, usuario_master)
-             VALUES ("Cervejaria Buteco","12312284567365","Fernando Master"),
-                    ("Lanche Bicicleta Unibratec","12312284567365","Renato Master"),
-                    ("Livraria do Lapis","12008284567365","Adriano Master"),
-                    ("Music Shopping","12312908567365","Rafael Master"),
-                    ("Crossfit da Morte","34512284567365","Juliane Master"),
-                    ("Motoca 123","12312282447365","Elton Master"),
-                    ("Salesforce do Aranha","99912284567365","Jedesson Master");
-                    
