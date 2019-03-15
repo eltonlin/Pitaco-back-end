2d8c99f14293b837570fa8baf6439c411d68b44f
FernandoBCAraujo <48303044+FernandoBCAraujo@users.noreply.github.com>
2019-03-15 01:24:18 -0300

    Delete scriptMaster.sql

--- scriptDeCarga/scriptMaster.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO usuario_master VALUES ("Fernando Master", "2233", "Fernando Araujo"),
-                                  ("Renato Master", "7777", "Renato Jales"),
-                                  ("Rafael Master", "1111", "Rafael Matos"),
-                                  ("Juliane Master", "2222", "Juliane Borba"),
-                                  ("Jedesson Master", "3333", "Jedesson Tavares"),
-                                  ("Adriano Master", "4444", "Adriano Jose"),
-                                  ("Elton Master", "5555", "Elton Linconl");
