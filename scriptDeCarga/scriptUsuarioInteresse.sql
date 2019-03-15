e3491dfe05d754ae97fc00824af4f0a276bf84de
FernandoBCAraujo <48303044+FernandoBCAraujo@users.noreply.github.com>
2019-03-15 01:23:54 -0300

    Delete scriptUsuarioInteresse.sql

--- scriptDeCarga/scriptUsuarioInteresse.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO usuario_final_interesse VALUES ("adriano", 8),
-                                           ("fernando", 6),
-                                           ("jedesson", 3),
-                                           ("joão", 7),
-                                           ("juliane", 2),
-                                           ("justin", 1),
-                                           ("rafael", 1),
-                                           ("renato", 5);
