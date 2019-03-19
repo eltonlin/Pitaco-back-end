6953d62d6e51491260b552a59891fa965ea993ea
Elton Linconl <eltonlinconl07@gmail.com>
2019-03-19 00:12:59 -0300

    Adicionando permiss ões de Acess-Control-Allow-Origin para a porta 8080

--- config/server.js
+++ config/server.js
@@ -22,6 +22,25 @@ app.use(express.static('./app/public'));
 /* configurar o middleware body-parser */
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 /* configurar o middleware express-validator */
 // app.use(expressValidator());
 
