4bc79b900b665618fa586c95782bee55ae50b7b9
Elton Linconl <eltonlinconl07@gmail.com>
2019-04-24 22:46:48 -0300

    Fazendo com que o interesse não cadastre mais duplamente

--- app/models/interesseDAO.js
+++ app/models/interesseDAO.js
@@ -14,12 +14,16 @@ interesseDAO.listarInteresses = function (result) {
     })
 };
 
-interesseDAO.inserirInteresse = function (interesse, resultado) {
-    connection.query('INSERT INTO INTERESSE SET ?', interesse, function (err, result) {
-        if (err) {
-            resultado({ mesage: ` Ocorreu um erro ao inserir o interesse ${interesse.descricao}` }, null);
-        }
-        resultado(null, { message: `O interesse ${interesse.descricao} foi cadastro com sucesso` });
+interesseDAO.inserirInteresse = function (interesse) {
+    return new Promise((resolve ,reject ) => {
+        connection.query('INSERT INTO INTERESSE SET ?', interesse, function (err, result) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        });
     })
 }
 
