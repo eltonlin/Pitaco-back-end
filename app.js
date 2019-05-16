fca2ae461449c3642137180a0cb22b9564c6e9a2
Elton Linconl <eltonlinconl07@gmail.com>
2019-05-16 20:26:56 -0300

    Criando rota inicial

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var app = require('./config/server');
 
 const PORT = process.env.port || 3000
 
+app.get("/", (req, res) => {
+    res.send("Hello to Pitaco Back-end");
+})
+
 app.listen(PORT, () => {
     console.log('escutando na porta ' + PORT);
 });
