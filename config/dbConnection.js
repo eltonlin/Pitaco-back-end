b1cda7b2f618b4466a9ef3d9d0a154460a230267
Elton Linconl <eltonlinconl07@gmail.com>
2019-05-16 21:15:12 -0300

    Alterando as config de dbconnection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "pitaco"
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  user: "badc03f739055a",
+  password: "dfbaa439",
+  database: "heroku_259349f41df4d5d"
 });
 
 connection.connect(function(err) {
