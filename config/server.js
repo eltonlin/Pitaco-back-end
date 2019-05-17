a47ce0df5ebcf1885acec6a610339a2ea5a77550
Elton Linconl <eltonlinconl07@gmail.com>
2019-05-16 22:38:29 -0300

    Adicionando permissão no CORS

--- config/server.js
+++ config/server.js
@@ -28,6 +28,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
 /* configurar o middleware express-validator */
 // app.use(expressValidator());
 
