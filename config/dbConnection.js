9df1e5946284bd666eccb8e2f898bdb871103de0
Elton Linconl <eltonlinconl07@gmail.com>
2019-05-17 23:07:29 -0300

    Adicionando o novo banco ao heroku

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,9 +2,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "us-cdbr-iron-east-02.cleardb.net",
-  user: "badc03f739055a",
-  password: "dfbaa439",
-  database: "heroku_259349f41df4d5d"
+  user: "b6eee132bd6e57",
+  password: "8d5cd05a",
+  database: "heroku_6465061891bfc99"
 });
 
 connection.connect(function(err) {
