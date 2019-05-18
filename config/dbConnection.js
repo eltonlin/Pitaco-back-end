1ff0465c27620d9296b3fb90f3b070236e2e5bc3
Elton Linconl <eltonlinconl07@gmail.com>
2019-05-18 13:03:33 -0300

    Tentando resolver problema de desconexão

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -20,14 +20,14 @@ function handleDisconnect() {
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
+  // connection.on('error', function(err) {
+  //   console.log('db error', err);
+  //   if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+  //     handleDisconnect();                         // lost due to either server restart, or a
+  //   } else {                                      // connnection idle timeout (the wait_timeout
+  //     throw err;                                  // server variable configures this)
+  //   }
+  // });
 }
 
 handleDisconnect();
