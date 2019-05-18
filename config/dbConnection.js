5d7e42b16efe252baba0c4edb423bc2e9974cd2e
Elton Linconl <eltonlinconl07@gmail.com>
2019-05-18 13:35:25 -0300

    Usando pool para abrir conexão

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,31 +1,14 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 
 var db_config  = {
+  connectionLimit: 10,
   host: "us-cdbr-iron-east-02.cleardb.net",
   user: "b6eee132bd6e57",
   password: "8d5cd05a",
   database: "heroku_6465061891bfc99"
 };
 
-var connection = mysql.createConnection(db_config);
-handleDisconnect(connection);
-
-function handleDisconnect(client) {
-  client.on('error', function (error) {
-    if (!error.fatal) return;
-    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
-
-    console.error('> Re-connecting lost MySQL connection: ' + error.stack);
-
-    // NOTE: This assignment is to a variable from an outer scope; this is extremely important
-    // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
-    // to `global.connection =` in node.
-    connection = mysql.createConnection(client.config);
-    handleDisconnect(connection);
-    connection.connect();
-  });
-};
-
+var connection = mysql.createPool(db_config);
 
 
 // var connection;
